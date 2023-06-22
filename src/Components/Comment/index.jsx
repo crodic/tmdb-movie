767cd712bc4afe6684e584f035d254d3d3d9d0cf
Crodic <113219638+Crodic@users.noreply.github.com>
2023-06-22 16:57:14 +0700

    das

--- src/Components/Comment/index.jsx
+++ src/Components/Comment/index.jsx
@@ -1,18 +1,10 @@
 import { useEffect } from "react";
 
 function Comment({ link }) {
-    useEffect(() => {
-        let scr = `<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v17.0&appId=674661591342580&autoLogAppEvents=1" nonce="f37gfIZZ"></script>`;
-        var scriptElement = document.createElement("script");
-        scriptElement.innerHTML = scr;
-        var bodyElement = document.querySelector("body");
-        bodyElement.appendChild(scriptElement);
-    }, []);
-
     return (
         <>
             <div
-                class="fb-comments"
+                className="fb-comments"
                 data-href={link}
                 data-width=""
                 data-numposts="10"
